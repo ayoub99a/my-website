07b44799dac8938bd2034e155d38fbc530298c99
ayoub99a <ayuob1999118a@gmail.com>
2022-10-24 14:58:15 +0300

    First Version

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Toggle Menu
+let toggleIcon = document.getElementById("toggleMenu");
+let navLink = document.getElementById("links");
+
+toggleIcon.addEventListener("click", function () {
+  toggleIcon.classList.toggle("opened");
+  navLink.classList.toggle("opened");
+});
+
+// Appear Cards in Services & Portfolio Sections
+let servicesSection = document.querySelector(".services");
+let portfolioSection = document.querySelector(".portfolio");
+
+window.addEventListener("scroll", function () {
+  if (this.scrollY > servicesSection.offsetTop - 150) {
+    appearCards("services");
+    if (this.scrollY > portfolioSection.offsetTop) {
+      // let servicesLink = document.querySelector("#links a");
+      // console.log(servicesLink.href);
+    }
+  }
+  if (this.scrollY > portfolioSection.offsetTop - 150) {
+    appearCards("portfolio");
+  }
+});
+
+function appearCards(section) {
+  let theTargetCards = document.querySelectorAll(`.${section} .card`);
+  theTargetCards.forEach((card) => {
+    card.style.opacity = "1";
+  });
+}
+
+// Scroll to top Button
+let scrollBtn = document.getElementById("scrollToTop");
+
+// Appear & Disappear
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 1000) {
+    scrollBtn.classList.add("appear");
+  } else {
+    scrollBtn.classList.remove("appear");
+  }
+});
+
+// The Functionality
+scrollBtn.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
